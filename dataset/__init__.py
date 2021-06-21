c83bbc118f9060511841a6bbdc85bda1b3d0b37c
Lance Wang <58240390+LanceWang12@users.noreply.github.com>
2021-06-21 22:31:19 +0800

    Delete dataset directory

--- dataset/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data_preprocessing import read_mat, train_test_split, get_pattern, get_dict
-from .dataset import bearing_dataset
-
-__all__ = ['read_mat', 'train_test_split', 'get_pattern', 'get_dict', 'bearing_dataset']
